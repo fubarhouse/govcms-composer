e30bf5641037c4bd7755feefdd2f8ccf8cda85f8
Karl Hepworth <karl.hepworth@gmail.com>
2018-03-18 13:14:36 +1100

    Add some basic debugging info.
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- rebuild.sh
+++ rebuild.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 if [[ -d ./docroot ]]; then
+  echo "Removing docroot folder.";
   rm -rf ./docroot;
 fi
 if [[ -d ./modules ]]; then
+  echo "Removing modules folder.";
   rm -rf ./modules;
 fi
 if [[ -d ./themes ]]; then
+  echo "Removing themes folder.";
   rm -rf ./themes
 fi
 if [[ -d ./libraries ]]; then
+  echo "Removing libraries folder.";
   rm -rf ./libraries;
 fi
 if [[ -a ./composer.lock ]]; then
+  echo "Removing composer lock file.";
   rm -f ./comoser.lock
 fi
 
+echo "Performing a fresh build...";
 composer install;
+exit 0;
